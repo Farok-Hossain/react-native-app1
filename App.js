bea9a858c0da7a6629f9ba79ea54111388afcabd
Farok Hossain <farokafs7@gmail.com>
2024-02-15 15:48:54 +0600

    added text

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>
+        Open up App.js to start working on your app
+      </Text>
+      <Text style={styles.blue}>blue</Text>
       <StatusBar style="auto" />
+      <View style={{ width: 100, height: 100, backgroundColor: "skyblue" }}>
+        <Text style={{ backgroundColor: "red" }}>Fuck you</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  red: {
+    color: "red",
+  },
+  blue: {
+    color: "blue",
   },
 });
